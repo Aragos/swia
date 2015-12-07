70382625771e324ea36565b69dea2cba0a5d46eb
Peter Schmitt <aragos@gmail.com>
2015-12-06 22:20:36 -0500

    Initial dice calculator UI

--- web/dice.js
+++ web/dice.js
@@ -1,43 +1,172 @@
 function setupDrops() {
-  $("#dice-source")
-      .find(".die")
+  makeElementsDraggable();
+  setupElementTrash();
+  setupTarget();
+  setupSurgeSource();
+  makeSurgeDraggable();
+}
+
+function makeElementsDraggable() {
+  $("#dice-source, #attack-modifiers, #defense-modifiers")
+      .find(".element")
           .draggable({
             appendTo: "body",
-            helper: "clone"
+            helper: "clone",
+            revert: "invalid", // jump back if not dropped on droppable
+            revertDuration: 200
           });
-  $("#dice-source")
+
+   $("#surge-source")
+      .find(".surge-element")
+            .draggable({
+              appendTo: "body",
+              helper: "clone",
+              revert: "invalid", // jump back if not dropped on droppable
+              revertDuration: 200
+            });
+}
+
+/**
+ * Allows any element to be dragged to any element source to remove it from the target or surge
+ * source.
+ */
+function setupElementTrash() {
+  $("#dice-source, #attack-modifiers, #defense-modifiers")
       .droppable({
-        accept: ".target-die",
+        accept: ".surge-element, .target-element",
         drop: function(event, dragged) {
-          dragged.draggable.remove();
-          if ($("#dice-target").find(".die").length == 0) {
-            $("#dice-placeholder").show();
+          var draggable = dragged.draggable;
+          if (draggable.hasClass("target-element")) {
+            draggable.remove();
+            updatePlaceholder();
+            updateProbabilities();
+          } else if (draggable.hasClass("surge-element")) {
+            if (draggable.hasClass("damage-modifier")) {
+              decreaseCount("#surge-damage-count");
+            } else if (draggable.hasClass("pierce-modifier")) {
+              decreaseCount("#surge-pierce-count");
+            } else if (draggable.hasClass("accuracy-modifier")) {
+              decreaseCount("#surge-accuracy-count");
+            }
           }
         }
       });
+}
 
-  $("#dice-target")
+function setupTarget() {
+  $("#target")
+        .droppable({
+          accept: function(element) {
+            return !element.hasClass("target-element") &&
+                (element.hasClass("element") || element.attr("id") == "surge-source");
+          },
+          drop: function(event, dragged) {
+            var newElement = dragged.draggable;
+            if (!newElement.hasClass("element")) { // it's a surge source, but we want the compiled surge
+              newElement = getCompiledSurge();
+              resetSurgeSource();
+            }
+            newElement
+                .clone()
+                .addClass("target-element")
+                .draggable({
+                  appendTo: "body",
+                  revert: "invalid", // jump back if not dropped on droppable
+                  revertDuration: 200
+                })
+                .appendTo(this);
+
+            updatePlaceholder();
+            updateProbabilities();
+          }
+        });
+}
+
+function setupSurgeSource() {
+  $("#surge-source")
       .droppable({
-        accept: ".source-die",
+        accept: ".damage-modifier, .pierce-modifier, .accuracy-modifier",
         drop: function(event, dragged) {
-          $("#dice-placeholder").hide();
-          dragged.draggable
-              .clone()
-              .removeClass("source-die")
-              .addClass("target-die")
-              .draggable({
-                appendTo: "body",
-                revert: "invalid", // jump back if not dropped on droppable
-                revertDuration: 200
-              })
-              .appendTo(this);
-          updateProbabilities();
+          var draggable = dragged.draggable;
+          if (draggable.hasClass("target-element")) {
+            draggable.remove();
+          }
+          if (draggable.hasClass("damage-modifier")) {
+            increaseCount("#surge-damage-count");
+          } else if (draggable.hasClass("pierce-modifier")) {
+            increaseCount("#surge-pierce-count");
+          } else if (draggable.hasClass("accuracy-modifier")) {
+            increaseCount("#surge-accuracy-count");
+          }
         }
+      });
+}
+
+function makeSurgeDraggable() {
+  $("#surge-source")
+      .draggable({
+        appendTo: "body",
+        revert: "invalid", // jump back if not dropped on droppable
+        revertDuration: 200,
+        helper: "clone"
+      });
+}
+
+/**
+ * Returns a copy of the current state of #surge-source as a compiled surge element.
+ */
+function getCompiledSurge() {
+  var compiled = $("#compiled-surge-template").clone();
+  compiled.find(".compiled-surge-damage-count").text($("#surge-damage-count").text().trim());
+  compiled.find(".compiled-surge-pierce-count").text($("#surge-pierce-count").text().trim());
+  compiled.find(".compiled-surge-accuracy-count").text($("#surge-accuracy-count").text().trim());
+  compiled.removeAttr("id");
+  return compiled;
+}
+
+function resetSurgeSource() {
+  $("#surge-damage-count").text("0");
+  $("#surge-pierce-count").text("0");
+  $("#surge-accuracy-count").text("0");
+}
+
+/**
+ * Updates the placeholder in the target area to show only if there are no elements in it.
+ */
+function updatePlaceholder() {
+  if ($("#target").find(".element").length == 0) {
+    $("#target-placeholder").show();
+  } else {
+    $("#target-placeholder").hide();
+  }
+}
+
+function setupRanged() {
+  $("#ranged-plus")
+      .click(function() {
+        increaseCount("#ranged-value");
+        updateProbabilities();
+      });
+
+  $("#ranged-minus")
+      .click(function() {
+        decreaseCount("#ranged-value");
+        updateProbabilities();
       });
 }
 
 function updateProbabilities() {
   var dice = getDice();
+  var modifiers = getModifiers();
+  var surgeAbilities = getSurgeAbilities();
+  var distance = getDistance();
+
+  console.log(dice);
+  console.log(modifiers);
+  console.log(surgeAbilities);
+  console.log(distance);
+
+  var damageByProbability = calculateDamage(dice, modifiers, surgeAbilities, distance);
 
   // TODO: Figure out what data is interesting, calculate probabilities
   // TODO: Display probabilities
@@ -49,9 +178,36 @@ function updateProbabilities() {
   });
   $("<div>" + diceString.substring(0, diceString.length-2) + "</div>").appendTo("#probabilities");
 }
+function getDistance() {
+  return parseInt($("#ranged-value").text());
+}
+
+function getSurgeAbilities() {
+  var surgeAbilities = [];
+  $("#target").find(".compiled-surge").each(function(index, compiledSurge) {
+    surgeAbilities.push({
+      "damage": parseInt($(compiledSurge).find(".compiled-surge-damage-count").text()),
+      "pierce": parseInt($(compiledSurge).find(".compiled-surge-pierce-count").text()),
+      "accuracy": parseInt($(compiledSurge).find(".compiled-surge-accuracy-count").text())
+    });
+  });
+  return surgeAbilities;
+}
+
+function getModifiers() {
+  var target = $("#target");
+  return {
+    "damage": target.find(".damage-modifier").length,
+    "pierce": target.find(".pierce-modifier").length,
+    "accuracy": target.find(".accuracy-modifier").length,
+    "surge": target.find(".surge-modifier").length,
+    "block": target.find(".block-modifier").length,
+    "evade": target.find(".evade-modifier").length
+  }
+}
 
 function getDice() {
-  var target = $("#dice-target");
+  var target = $("#target");
   return {
     "black": target.find(".black-die").length,
     "blue": target.find(".blue-die").length,
@@ -59,5 +215,30 @@ function getDice() {
     "red": target.find(".red-die").length,
     "white": target.find(".white-die").length,
     "yellow": target.find(".yellow-die").length
+  }
+}
+
+/**
+ * Increases the numerical text of the DOM element with the given ID by one. Assumes the DOM
+ * element has a numerical text to begin with.
+ */
+function increaseCount(id) {
+  $(id).text(parseInt($(id).text()) + 1);
+}
+
+/**
+ * Decreases the numerical text of the DOM element with the given ID by one, as long as the result
+ * is non-negative. Assumes the DOM element has a numerical text to begin with.
+ */
+function decreaseCount(id) {
+  $(id).text(Math.max(0, parseInt($(id).text()) - 1));
+}
+
+function calculateDamage(dice, modifiers, surgeAbilities, distance) {
+  return {
+    1:.91,
+    2: .9,
+    3: .6,
+    4: .25
   }
 }

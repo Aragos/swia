aa23b8d698b86ea7a434389c899707260b03cefb
Peter Schmitt <aragos@gmail.com>
2015-12-20 21:40:43 -0500

    first chart version based on highcharts

--- web/dice.js
+++ web/dice.js
@@ -156,29 +156,60 @@ function setupRanged() {
       });
 }
 
+function setupChart() {
+  damageChart = $('#chart').highcharts({
+    credits: false,
+    chart: {
+      borderRadius: 5,
+      spacing: [8, 2, 2, 2],
+    },
+    title: {
+      text: null,
+    },
+    yAxis: {
+      title: {
+        text: null,
+      },
+      labels: {
+        x: 2,
+
+      }
+    },
+    tooltip: {
+      valueSuffix: '%',
+    },
+    legend: {
+      enabled: false,
+    },
+    series: [{
+      id: 'damage',
+      name: 'Damage',
+      data: []
+    }, {
+      id: 'surge',
+      name: 'Extra Surge',
+      data: []
+    }]
+  });
+}
+
 function updateProbabilities() {
   var dice = getDice();
   var modifiers = getModifiers();
   var surgeAbilities = getSurgeAbilities();
   var distance = getDistance();
 
-  console.log(dice);
-  console.log(modifiers);
-  console.log(surgeAbilities);
-  console.log(distance);
-
-  var damageByProbability = calculateDamage(dice, modifiers, surgeAbilities, distance);
+  var probabilitiesByDamage = calculateDamage(dice, modifiers, surgeAbilities, distance);
 
   // TODO: Figure out what data is interesting, calculate probabilities
-  // TODO: Display probabilities
 
-  $("#probabilities").empty();
-  var diceString = "";
-  $.each(dice, function(color, count) {
-    diceString += color + ": " + count + ", ";
-  });
-  $("<div>" + diceString.substring(0, diceString.length-2) + "</div>").appendTo("#probabilities");
+  var chartData = damageToChartData(probabilitiesByDamage);
+
+  var damageChart = $("#chart").highcharts();
+  damageChart.get('damage').setData(chartData.damageData);
+  damageChart.get('surge').setData(chartData.surgeData);
 }
+
 function getDistance() {
   return parseInt($("#ranged-value").text());
 }
@@ -235,6 +266,24 @@ function decreaseCount(id) {
   $(id).text(Math.max(0, parseInt($(id).text()) - 1));
 }
 
+function damageToChartData(probabilitiesByDamage) {
+  var damageData = [];
+  var surgeData = [];
+  $.each(probabilitiesByDamage.sort(compareByCount), function(_, probabilities) {
+    damageData.push([probabilities.label, Math.round(probabilities.damage * 100)]);
+    surgeData.push([probabilities.label, Math.round(probabilities.surge * 100)]);
+  });
+
+  return {
+    damageData: damageData,
+    surgeData: surgeData
+  };
+}
+
+function compareByCount(a, b) {
+  return a.count - b.count;
+}
+
 /**
  * Calculates the cumulative probability for at least N damage, for all damage counts possible with
  * the given dice and modifiers.
@@ -244,14 +293,19 @@ function decreaseCount(id) {
  * @param surgeAbilities array of surge ability objects, each consuming a single surge and yielding
  *    the specified modifiers (modifier name -> int)
  * @param distance int representing the ranged distance to target
- * @returns damage count (int) to damage probability mapping (float between 0 and 1)
+ * @returns an array containing objects with three fields eache, "count" (int), "damage", the
+ *    cumulative probability (float between 0 and 1) to deal at least "count" damage and "surge",
+ *    the probability (float between 0 and 1) of having an excess surge if exactly "count" damage is
+ *    dealt
  */
 function calculateDamage(dice, modifiers, surgeAbilities, distance) {
+
   // TODO: Implement.
-  return {
-    1:.91,
-    2: .9,
-    3: .6,
-    4: .25
-  }
+
+  return [
+    { count: 1, damage: Math.random(), surge: Math.random() * .2 },
+    { count: 3, damage: Math.random(), surge: Math.random() * .2 },
+    { count: 2, damage: Math.random(), surge: Math.random() * .2 },
+    { count: 4, damage: Math.random(), surge: Math.random() * .2 }
+  ];
 }
